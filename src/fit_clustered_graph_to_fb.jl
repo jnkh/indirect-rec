5336c6f695f1177766af33f7cb6c97de700b98b3
Julian Kates-Harbeck <juliankh@rclogin09.rc.fas.harvard.edu>
2016-03-30 19:58:14 -0400

    parameter change

--- src/fit_clustered_graph_to_fb.jl
+++ src/fit_clustered_graph_to_fb.jl
@@ -3,6 +3,6 @@ using RandomClusteringGraph, Distributions, LightGraphs, GraphCreation
 H = create_graph(4040,44,:fb)
 d = fit(Pareto,degree(H))
 N = 4040
-C = 0.6
-G = random_clustering_graph(d,N,C) 
+C = 0.8
+G = random_clustering_graph(d,N,C,false)
 println("Finished")
