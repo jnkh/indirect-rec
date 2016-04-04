cfd8b2987db727a04a4da3e8d1bb14649973a69b
Julian Kates-Harbeck <juliankh@rclogin08.rc.fas.harvard.edu>
2016-04-04 01:12:11 -0400

    fitting

--- src/fit_clustered_graph_to_fb.jl
+++ src/fit_clustered_graph_to_fb.jl
@@ -3,6 +3,6 @@ using RandomClusteringGraph, Distributions, LightGraphs, GraphCreation
 H = create_graph(4040,44,:fb)
 d = fit(Gamma,degree(H))
 N = 4040
-C = 0.8
-G = random_clustering_graph(d,N,C,false)
+C = 0.79
+G = random_clustering_graph(d,N,C,false,"fb_to_gamma_fit_clustering.dat")
 println("Finished")
